6047a88578dc33e4a3c9a61fc6f0d727ecf62619
Solymosi Norbert <solymosi.norbert@gmail.com>
2018-02-28 09:15:59 +0100

    ##------ Wed Feb 28 09:15:59 2018 ------##
    sessionInfo:
    R version 3.4.3 (2017-11-30)
    Platform: x86_64-pc-linux-gnu (64-bit)
    Running under: Ubuntu 16.04.4 LTS
    
    Matrix products: default
    BLAS: /usr/local/lib/R/lib/libRblas.so
    LAPACK: /usr/local/lib/R/lib/libRlapack.so
    
    locale:
     [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C              
     [3] LC_TIME=hu_HU.UTF-8        LC_COLLATE=en_US.UTF-8    
     [5] LC_MONETARY=hu_HU.UTF-8    LC_MESSAGES=en_US.UTF-8   
     [7] LC_PAPER=hu_HU.UTF-8       LC_NAME=C                 
     [9] LC_ADDRESS=C               LC_TELEPHONE=C            
    [11] LC_MEASUREMENT=hu_HU.UTF-8 LC_IDENTIFICATION=C       
    
    attached base packages:
    [1] stats     graphics  grDevices utils     datasets  methods   base     
    
    other attached packages:
    [1] git2r_0.21.0
    
    loaded via a namespace (and not attached):
    [1] compiler_3.4.3

---
+++ R/OIEparser-internal.R
@@ -0,0 +1,43 @@
+.oielinker <-
+function(ystart, yend){
+    no = (yend-ystart+1)*12
+    pb = progress_bar$new(total=no)
+    htms = c()
+    for(y in ystart:yend){
+        for(m in 1:12){
+            h = new_handle()
+            handle_setopt(h, customrequest = "POST")
+            handle_setform(h, pdf_report_type="imm", month=as.character(m), year=as.character(y))
+            r = curl_fetch_memory('http://www.oie.int/wahis_2/public/wahid.php/Diseaseinformation/reportarchive', h)
+            htm = rawToChar(r$content)
+            htm = strsplit(htm, '\n')[[1]]
+            htms = c(htms, htm[which(grepl('reports/en_', htm))])    
+#             print(paste(y, m, sep='/'))
+            pb$tick()
+            Sys.sleep(1/no)
+        }
+    }
+    links = substr(htms, 46, 136-22)
+    pdfs = paste('en_', matrix(unlist(strsplit(links, 'en_')), nc=2, byrow=T)[,2], sep='')
+    pdfs[which(substr(pdfs,37-3,37)!='.pdf')] = paste(pdfs[which(substr(pdfs,37-3,37)!='.pdf')], 'pdf', sep='')
+    urls = paste('http://www.oie.int/wahis_2/temp/reports/', pdfs, sep='')
+    return(urls)
+}
+.oiedl <-
+function(urls, timeout=10000){
+    no = length(urls)
+    pb = progress_bar$new(total=no)
+    report.lst = list()
+    for(i in 1:no){
+        h = new_handle()
+        handle_setopt(h, connecttimeout_ms=timeout, timeout_ms=timeout)        
+        url = urls[i]
+        tmp = tempfile()
+        curl_download(url, tmp, handle=h)
+        u = pdf_text(tmp)
+        report.lst[[i]] = u
+        pb$tick()
+        Sys.sleep(1/no)
+    }
+    return(report.lst)
+}
